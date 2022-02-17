76efe56b18aafd30995d69e13bae8589d075b68f
Aleksandr-Trenohin <aleksandr_trenohin@bigmir.net>
2022-02-17 23:30:40 +0200

    add task-2 solution

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,47 @@
+import throttle from "lodash.throttle";
+
+// 3
+const iframe = document.querySelector("iframe");
+const player = new Vimeo.Player(iframe);
+
+// 4
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+  console.log(data);
+  onTimeupdate(data);
+};
+
+player.on("timeupdate", throttle(onPlay, 1000)); // 7
+
+// 5
+function onTimeupdate(evt) {
+    const playTime = evt.seconds;
+ 
+  localStorage.setItem("videoplayer-current-time", playTime);
+}
+
+function gettingTime() {
+    const savedTime = +localStorage.getItem("videoplayer-current-time");
+  
+    if (savedTime) {
+    console.log(savedTime);
+    // 6
+    player
+      .setCurrentTime(savedTime)
+      .then(function (seconds) {
+        // seconds = the actual time that the player seeked to
+      })
+      .catch(function (error) {
+        switch (error.name) {
+          case "RangeError":
+            // the time was less than 0 or greater than the video’s duration
+            break;
+          default:
+            // some other error occurred
+            break;
+        }
+      });
+  }
+}
+gettingTime();
 
